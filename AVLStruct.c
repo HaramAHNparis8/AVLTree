86508b7a30a40618b46b9c6a458dceadfdd65d8f
AHN Haram <160437590+HaramAHNparis8@users.noreply.github.com>
2025-01-28 00:08:48 +0900

    로테이션 및 AVL 세팅 함수 구현

--- AVLStruct.c
+++ AVLStruct.c
@@ -1,5 +1,91 @@
 #include <stdio.h>
 #include AVLStruct.h
 #include <stdlib.h>
+ 
 
+element_t Height(Node_t* node){
+
+    element_t leftDepth, rightDepth;    
+
+    if (node == NULL){ 
+		
+		return 0;
+	}
+	
+    
+    leftDepth = Height(node->Left);
+    rightDepth = Height(node->Right);
+	
+    return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+}
+
+element_t CalculateBalanceFactor(Node* node){
+
+    return Height(node->Left) - Height(node->Right);
+
+}
+
+Node* LL(Node* node){
+
+    Node* childNode = node -> L;
+    node -> Left = childNode -> R;
+    if (childNode -> R != NULL){
+        
+	childNode -> R -> Parent = node;
+    }
+	
+    childNode -> R = node;
+    childNode -> Parent = node -> Parent;
+    node->Parent = childNode;
+	
+    return childNode;
+}
+
+
+Node* LR(Node* node){
+
+    node -> L = RR(node -> L);
+    return LL(node);
+}
+
+Node* RL(Node* node){
+
+    node -> R = LL( node -> R);
+    return RR(node);
+}
+Node* AVLSet(Node* node){
+
+    int depth = CalculateBalanceFactor(node);
+    if (depth >= 2)
+    {
+        depth = CalculateBalanceFactor(node->Left);
+        if (depth >= 1)
+        {
+            //LL : Left Left
+            node = LL(node);
+        }
+        else
+        {
+            //LR : Left Right
+            node = LR(node);
+        }
+    }
+    else if (depth <= -2)
+    {
+        depth = CalculateBalanceFactor(node->Right);
+        if (depth <= -1)
+        {
+            //RR : Right Right
+            node = RR(node);
+        }
+        else
+        {
+            //RL : Right Left
+            node = RL(node);
+        }
+    
+    }
+	
+    return node;
+}
 
